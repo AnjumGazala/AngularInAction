feb3ece10967c8952dba9a0ce3ec86522fe23779
AnjumGazala <gazalak528@gmail.com>
2025-01-15 16:58:59 +0530

    Property and Event Binding

--- demo-app-older/src/app/product-list/product-list.component.ts
+++ demo-app-older/src/app/product-list/product-list.component.ts
@@ -6,14 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  addToCart: number = 0
+  //name ="Jhon Doe"
   product = {
-    name: "iphone 13",
+    name: "iphone X",
     price: 999,
     color: "Black",
     discount: 8.5,
-    instock: 0
+    instock: 10,
+    pImage: "/assets/images/iphone.jpg"
   }
   getDiscountedPrice(){
     return this.product.price - (this.product.price * this.product.discount /100)
+  }
+  onNameChange(event:any){
+    //console.log(event.target.value);
+    //this.name = event.target.value
+  }
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart --;
+    }
+    
+  }
+  incrementCartValue(){
+    if(this.addToCart < this.product.instock){
+      this.addToCart ++;
+    }
+    
   }
 }
